7debe23461e0da1daebfc57c8c9f8401532f1445
akalibekova <dairako2000@gmail.com>
2023-02-15 13:10:10 +0600

    Add AllureReports

--- src/test/java/reqres/specs/Specs.java
+++ src/test/java/reqres/specs/Specs.java
@@ -6,16 +6,24 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 import static io.restassured.RestAssured.with;
+import static io.restassured.filter.log.LogDetail.BODY;
+import static io.restassured.filter.log.LogDetail.STATUS;
+import static reqres.helpers.CustomApiListener.withCustomTemplates;
 
 public class Specs {
     public static RequestSpecification requestSpec = with()
             .baseUri("https://reqres.in")
             .basePath("/api")
-            .log().all()
+            .log().uri()
+            .log().headers()
+            .log().body()
+            .filter(withCustomTemplates())
             .contentType(ContentType.JSON);
 
     public static ResponseSpecification responseSpec(int status) {
         return new ResponseSpecBuilder()
+                .log(STATUS)
+                .log(BODY)
                 .expectStatusCode(status)
                 .build();
     }
